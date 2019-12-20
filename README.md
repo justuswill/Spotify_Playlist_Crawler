1f2ea4d9219e25184b994c99cc612d628b77833b
Justus <justuswill@gmail.com>
2019-12-20 13:43:17 +0100

    Collections

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Spotify_Playlist_Crawler
-A Crawler that keeps track of an public playlist and can be invoked to look what songs changed since the last time.
-It than creates an new playlist with all new songs in your account.
+A Crawler that keeps track of an public playlists and can be invoked to look what songs changed since the last time.
+It than creates a new playlist with all new songs in your account.
 The first time you use it you have to sign in to your account by following a link and copying a URL. Details are in the terminal.
 If no URL/URI is giving it uses the last recently used one
+You can also create Collections of playlist to keep track of multiple playlists
 
 Requires:
 Python 3
@@ -21,15 +22,26 @@ check_playlist.py --help
 >   -h, --help       show this help message and exit
 >   --URI URI        Playlist URI
 >   --URL URL        Playlist URL for an public playlist
+>   --col COL        Playlist Collection for an public playlist
 >   --create CREATE  Create a playlist from the new tracks
+>   --user USER      Use the account specified by this username
+>   --add ADD        add the uri/url to a collection with this name
+
 ```
 
 ```
 python check_playlist.py --URL 'https://open.spotify.com/playlist/37i9dQZF1DX8AliSIsGeKd?si=n5K8tOzXSwGg8yMcDBihUQ'
-> Username:<enter_your_username>
 > Playlist 'Electronic Rising Dec-06-2019' created'
 ```
-
+Add a track to a collection with a specified name
+```
+python check_playlist.py --URL 'https://open.spotify.com/playlist/37i9dQZF1DX8AliSIsGeKd?si=n5K8tOzXSwGg8yMcDBihUQ' --add electro
+> Everything up to date
+```
+```
+python check_playlist.py --col electro
+> Everything up to date
+```
 ```
 python check_playlist.py
 > Everything up to date
--- check_playlist.py
+++ check_playlist.py
@@ -6,84 +6,122 @@ import spotipy.util as util
 import pickle
 from datetime import date
 
-if __name__ == '__main__':
+
+def main():
     parser = ap.ArgumentParser(description='Check for a playlist-URL or URI, which entries are new since the last check'
                                            'those are added to a newly created playlist in your account')
     parser.add_argument('--URI', default=None, help='Playlist URI')
     parser.add_argument('--URL', default=None, help='Playlist URL for an public playlist')
+    parser.add_argument('--col', default=None, help='Playlist Collection for an public playlist')
     parser.add_argument('--create', default=True, help='Create a playlist from the new tracks')
+    parser.add_argument('--user', default=None, help='Use the account specified by this username')
+    parser.add_argument('--add', default=None, help='add the uri/url to a collection with this name')
     args = parser.parse_args()
 
     client_credentials_manager = SpotifyClientCredentials(client_id='f6a685b72991467fbb9b45ca9a39ffaf', client_secret='c0d27814ffc84b289ca0611f3dae6c25')
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    uri = args.URI
-    url = args.URL
-    if uri is not None:
-        username = 'spotify'
-        playlist_id = uri.split(':')[-1]
-        with open("latest.pickle", "wb") as pickle_out:
-            pickle.dump([username, playlist_id], pickle_out)
-    elif url is not None:
-        username = 'spotify'
-        playlist_id = url.split('/')[-1].split('?')[0]
-        with open("latest.pickle", "wb") as pickle_out:
-            pickle.dump([username, playlist_id], pickle_out)
-    else:
+
+    # Load latest
+    try:
         pickle_in = open("latest.pickle", "rb")
-        [username, playlist_id] = pickle.load(pickle_in)
+    except FileNotFoundError:
+        with open("latest.pickle", "wb") as pickle_out:
+            pickle.dump([None, None, None, dict()], pickle_out)
+    pickle_in = open("latest.pickle", "rb")
+    [pl_username, playlist_id, username, cols] = pickle.load(pickle_in)
+
+    if args.col is not None:
+        if args.col not in cols.keys():
+            print("No Collection with this name")
+            return
+        pl_list = cols[args.col]
+    elif args.URI is not None:
+        pl_username = 'spotify'
+        playlist_id = args.URI.split(':')[-1]
+        pl_list = [(pl_username, playlist_id)]
+    elif args.URL is not None:
+        pl_username = 'spotify'
+        playlist_id = args.URL.split('/')[-1].split('?')[0]
+        pl_list = [(pl_username, playlist_id)]
+    if args.user is not None:
+        username = args.user
+    if username is None:
+        username = input('Username:')
+    if playlist_id is None or pl_username is None:
+        print('No Playlist specified')
+        return
+
+    # add to collection
+    collection_name = args.add
+    if collection_name is not None:
+        if collection_name not in cols.keys():
+            cols[collection_name] = []
+        cols[collection_name] += [(pl_username, playlist_id)]
+    #Save latest
+    with open("latest.pickle", "wb") as pickle_out:
+        pickle.dump([pl_username, playlist_id, username, cols], pickle_out)
 
     # load old songs
     old_songs = pd.read_csv('database.csv', index_col=0)
 
-    # Get all tracks
-    results = sp.user_playlist_tracks(username, playlist_id)
-    tracks = results['items']
-    while results['next']:
-        results = sp.next(results)
-        tracks.extend(results['items'])
+    for pl_username, playlist_id in pl_list:
+        # Get all tracks
+        results = sp.user_playlist_tracks(pl_username, playlist_id)
+        tracks = results['items']
+        while results['next']:
+            results = sp.next(results)
+            tracks.extend(results['items'])
 
-    # Create df for new songs
-    artists = []
-    names = []
-    uris = []
-    for i, track in enumerate(tracks):
-        try:
-            art = [track['track']['artists'][0]['name']]
-            nam = [track['track']['name']]
-            ur = [track['track']['uri']]
-        except TypeError:
-            print('Fehlerhafter Track %d' % i)
-            continue
-        artists += art
-        names += nam
-        uris += ur
-    new_songs = pd.DataFrame({'artist': artists, 'name': names, 'uri': uris, 'playlist_id': len(names) * [playlist_id]})
+        # Create df for new songs
+        artists = []
+        names = []
+        uris = []
+        for i, track in enumerate(tracks):
+            try:
+                art = [track['track']['artists'][0]['name']]
+                nam = [track['track']['name']]
+                ur = [track['track']['uri']]
+            except TypeError:
+                print('Fehlerhafter Track %d' % i)
+                continue
+            artists += art
+            names += nam
+            uris += ur
+        new_songs = pd.DataFrame({'artist': artists, 'name': names, 'uri': uris, 'playlist_id': len(names) * [playlist_id]})
 
-    # Get new tracks
-    useful_old_songs = old_songs.loc[old_songs['playlist_id'] == playlist_id]
-    to_add = new_songs.loc[new_songs['uri'].isin(useful_old_songs['uri'].values) == False]
-    old_songs = old_songs.append(to_add)
+        # Get new tracks
+        # Use all
+        # useful_old_songs = old_songs.loc[old_songs['playlist_id'] == playlist_id]
+        useful_old_songs = old_songs
+        to_add = new_songs.loc[new_songs['uri'].isin(useful_old_songs['uri'].values) == False]
+        old_songs = old_songs.append(to_add)
 
-    # Save current state
-    old_songs.to_csv('database.csv')
-    if args.create == True:
-        if len(to_add) > 0:
-            # Auth
-            scope = 'playlist-modify-public'
-            username = input('Username:')
-            token = util.prompt_for_user_token(username, scope, client_id='f6a685b72991467fbb9b45ca9a39ffaf',
-                                               client_secret='c0d27814ffc84b289ca0611f3dae6c25', redirect_uri='http://127.0.0.1')
+        # Save current state
+        old_songs.to_csv('database.csv')
+        if args.create == True:
+            if len(to_add) > 0:
+                # Auth
+                scope = 'playlist-modify-public'
+                token = util.prompt_for_user_token(username, scope, client_id='f6a685b72991467fbb9b45ca9a39ffaf',
+                                                   client_secret='c0d27814ffc84b289ca0611f3dae6c25', redirect_uri='http://127.0.0.1')
 
-            if token:
-                sp = spotipy.Spotify(auth=token)
-                sp.trace = False
-                # Create a playlist with new songs
-                pl = sp.user_playlist(username, playlist_id)
-                pl_name = pl['name'] + date.today().strftime(" %b-%d-%Y")
-                created_playlist = sp.user_playlist_create(username, pl_name)
-                sp.user_playlist_add_tracks(username, created_playlist['uri'], to_add['uri'].values)
-                print('Playlist \'%s\' created' % pl_name)
+                if token:
+                    sp = spotipy.Spotify(auth=token)
+                    sp.trace = False
+                    # Create a playlist with new songs
+                    pl = sp.user_playlist(username, playlist_id)
+                    pl_name = pl['name'] + date.today().strftime(" %b-%d-%Y")
+                    created_playlist = sp.user_playlist_create(username, pl_name)
+                    to_add_songs = to_add['uri'].values
+                    while len(to_add_songs) > 0:
+                        sp.user_playlist_add_tracks(username, created_playlist['uri'], to_add_songs[:90])
+                        to_add_songs = to_add_songs[90:]
+                    print('Playlist \'%s\' created' % pl_name)
+                else:
+                    print("Can't get token for", username)
             else:
-                print("Can't get token for", username)
-        else:
-            print('Everything up to date')
+                print('Everything up to date - %s' % playlist_id)
+
+
+if __name__ == '__main__':
+    main()
